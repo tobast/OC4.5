d05a8cc5f12dd20418b7a64602f30e1cefa2bdfb
Noémie Fong <noemie.fong@laposte.net>
2016-03-17 19:20:38 +0100

    Added random training set generator (test_functions)

---
+++ src/test_functions.ml
@@ -0,0 +1,55 @@
+open DataTypes
+open Random
+
+
+(* This is a file to test the functions of c45.ml*)
+
+let rec genUnifList nb = function
+  | 0 -> []
+  | len -> nb :: (genUnifList nb (len-1))
+
+let nbCategories = 3 
+
+let nbFeatures = 6 
+
+let nbVal = 10 (* for each category *)
+
+let uncertainty = [|40; 1000; 400; 300; 1000; 400|]
+
+let featContinuity = [|true; true; true; true; true; true|]
+
+let canonCat1 = [|120; 6083; 3049; 734; 3052; 1363 |]
+and canonCat2 = [|468; 1309; 3209; 590; 4029; 1095 |]
+and canonCat3 = [|58; 4968; 2798; 409; 4672; 1299 |]
+
+let canon = [|canonCat1; canonCat2; canonCat3|]
+
+let featureMax = [|550; 7500; 4000; 1200; 6000; 2000|]
+
+let genRandom () = 
+  let set = ref [] in
+  for cat = 1 to nbCategories do
+    for count_val = 1 to nbVal do
+      let data = Array.make nbFeatures 0 in 
+      for feat = 0 to (nbFeatures - 1) do
+        let canonVal = canon.(cat-1).(feat) in
+        let rand = Random.int ( 2 * uncertainty.(feat)) in
+        let value = canonVal + rand - uncertainty.(feat) in
+        data.(feat) <- value ;
+      done ;
+      set := { data = data ; category = cat } :: !set ;
+    done ;
+  done ;
+  !set
+
+
+let randTrainSet () = 
+{
+  set = genRandom ();
+  nbFeatures = nbFeatures ;
+  featureMax = featureMax;
+  featContinuity = featContinuity ;
+  nbCategories = nbCategories ;
+  setSize = nbVal * nbCategories;
+}
+
