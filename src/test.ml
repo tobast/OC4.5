20bcda4350e16defc1c57bea0b42bb93dd4de160
Théophile Bastian <contact@tobast.fr>
2016-03-18 22:00:07 +0100

    Modified to run a few classification tests.

--- src/test.ml
+++ src/test.ml
@@ -1,10 +1,19 @@
 open Oc45
 open Test_functions
 
-let testedCat = 0
+let () = Random.self_init ()
 
 let tree = c45 (randTrainSet ())
-let data = testData testedCat
-let () = if classify tree data != testedCat then failwith "Test failure"
+
+let nbTests = 10000
+let failed = ref 0
+let () = for test = 0 to nbTests-1; do
+	let cat = Random.int nbCategories in
+	let data = testData cat in
+	if classify tree data <> cat then
+		failed := !failed + 1
+done
 
 let () = toDotStdout tree
+
+let () = Format.eprintf "Failed %d/%d tests.@." !failed nbTests
