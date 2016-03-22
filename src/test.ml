0192c164d80e435c045d08b9708e0b3b37befb69
Théophile Bastian <contact@tobast.fr>
2016-03-22 23:18:51 +0100

    Adapted test functions/main

--- src/test.ml
+++ src/test.ml
@@ -3,17 +3,17 @@ open Test_functions
 
 let () = Random.self_init ()
 
-let tree = c45 (randTrainSet ())
+let tree = IntOc45.c45 (randTrainSet ())
 
 let nbTests = 10000
 let failed = ref 0
 let () = for test = 0 to nbTests-1; do
 	let cat = Random.int nbCategories in
 	let data = testData cat in
-	if classify tree data <> cat then
+	if IntOc45.classify tree data <> cat then
 		failed := !failed + 1
 done
 
-let () = toDotStdout tree
+let () = IntOc45.toDotStdout (fun fmt x -> Format.fprintf fmt "%d" x) tree
 
 let () = Format.eprintf "Failed %d/%d tests.@." !failed nbTests
--- src/test_functions.ml
+++ src/test_functions.ml
@@ -1,7 +1,6 @@
 open Random
 open Oc45
 
-
 (* This is a file to test the functions of c45.ml*)
 
 (* NOTE Warning! Random *MUST* be inited in the calling file. *)
@@ -32,28 +31,30 @@ let genRandom () =
   let set = ref [] in
   for cat = 1 to nbCategories do
     for count_val = 1 to nbVal do
-      let data = Array.make nbFeatures 0 in 
+      let data = Array.make nbFeatures (IntOc45.Continuous(0)) in 
       for feat = 0 to (nbFeatures - 1) do
         let canonVal = canon.(cat-1).(feat) in
         let rand = Random.int ( 2 * uncertainty.(feat)) in
         let value = canonVal + rand - uncertainty.(feat) in
-        data.(feat) <- value ;
+        data.(feat) <- IntOc45.Continuous(value) ;
       done ;
-      set := { data = data ; category = cat - 1 } :: !set ;
+      set := IntOc45.({ data = data ; category = cat - 1 }) :: !set ;
+	  
     done ;
   done ;
   !set
 
 
 let randTrainSet () = 
-	addDataList (genRandom ())
-		(emptyTrainSet nbFeatures nbCategories featContinuity)
+	IntOc45.addDataList (genRandom ())
+		(IntOc45.emptyTrainSet nbFeatures nbCategories featContinuity)
 
 let testData n = 
-    let data = Array.make nbFeatures 0 in
+    let data = Array.make nbFeatures (IntOc45.Continuous(0)) in
     for feat = 0 to (nbFeatures - 1) do
         let rand = Random.int (2* uncertainty.(feat)) in
-        data.(feat) <- canon.(n).(feat) + rand - uncertainty.(feat);
+        data.(feat) <- IntOc45.Continuous(
+			canon.(n).(feat) + rand - uncertainty.(feat));
     done ;
     data 
         
